025d027edabe558e2764084e7adc643a11231885
wesd724 <wesd723@naver.com>
2019-08-31 14:46:37 +0900

    study

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -19,3 +19,21 @@ function notag() {
 document.getElementById("NoTag").style.color="yellow";
 
 }
+
+var asd = {a: 5, b: 7, c: 1};
+for (var i in asd) {
+    console.log(i + ": " + asd[i]);
+}
+
+
+try {
+    var qwe = ["a", 1, 67, "한국", 87.5];
+    for(var i in qwe) {
+        console.log(i + ": " + qwe[i]);
+    }
+} catch(e) {
+        console.log("오류인 배열 요소");
+  }
+
+  
+
